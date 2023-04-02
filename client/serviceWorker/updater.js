97198aabc6facf79338d2c7c89549ac455b2caa7
GooseOb <GooseOb@mail.ru>
2023-04-02 03:33:34 +0200

    improve changes checker in service worker cache updater

--- client/serviceWorker/updater.js
+++ client/serviceWorker/updater.js
@@ -22,23 +22,30 @@ readline.question('Update service worker cache? (y/n): ', async answer => {
         console.log('No cache updated');
         return;
     }
-    const {stdout: changedFilePaths} = await exec('git diff --name-only');
-    const updateSuggestion = name => changeChecks[name].test(changedFilePaths)
-        ? '<- uncommitted changes' : '';
+    const {stdout: gitDiffFilePaths} = await exec('git diff --name-only');
     const cacheNames = Object.keys(cacheConfig);
-    const options = cacheNames
-        .map((name, i) => `${i}. ${name} ${updateSuggestion(name)}`)
+    const gitDiffCacheNames = cacheNames.filter(name => changeChecks[name].test(gitDiffFilePaths));
+    const getSuggestion = name => gitDiffCacheNames.includes(name) ? '<- uncommitted changes' : '';
+    let options = cacheNames
+        .map((name, i) => `${i}. ${name} ${getSuggestion(name)}`)
         .join('\n');
+    if (gitDiffCacheNames.length) options += '\nx. all uncommitted';
     readline.question(`Choose caches to update (any separator)\n${options}\n> `, answer => {
         readline.close();
-        const ids = answer.match(/\d/g); // change to /\d+/g if options amount more than 10
-        if (!ids) {
-            console.log('No option chosen');
-            return;
+        answer = answer.trim();
+        let updatedCacheNames;
+        if (answer === 'x' || answer === 'X') {
+            updatedCacheNames = gitDiffCacheNames;
+        } else {
+            const ids = answer.match(/\d/g); // change to /\d+/g if options amount more than 10
+            if (!ids) {
+                console.log('No option chosen');
+                return;
+            }
+            updatedCacheNames = ids
+                .map(id => cacheNames[id])
+                .filter(Boolean);
         }
-        const updatedCacheNames = ids
-            .map(id => cacheNames[id])
-            .filter(Boolean);
         for (const cacheName of updatedCacheNames) {
             const cache = cacheConfig[cacheName];
             cache.v = String(+cache.v + 1);
@@ -46,7 +53,7 @@ readline.question('Update service worker cache? (y/n): ', async answer => {
         writeFile(
             filePath,
             JSON.stringify(cacheConfig, null, 2),
-            ()=>{console.log('Updated successfully: ' + updatedCacheNames.join(', '))}
+            ()=>{console.log('Updated successfully: ' + updatedCacheNames.join(', ') || 'nothing')}
         );
     })
 });
