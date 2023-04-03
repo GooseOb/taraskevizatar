650b2477ff730d7b93c3e3b2ebc629d63d440fc3
nopears <gregorzhilich@gmail.com>
2023-04-03 20:14:16 +0300

    Added CORS policy to API and removed semicolons from prettier

--- api/api.ts
+++ api/api.ts
@@ -1,18 +1,21 @@
-import {Tarask, Promisify} from "../scripts/taraskTypes";
+import { Tarask, Promisify } from '../scripts/taraskTypes';
 
-const tarask: Promisify<Tarask> = (text, isHtml, {abc: alphabet, j: alwaysJ}) => {
-    console.log(text);
-    return fetch('http://localhost:3000/', {
-        method: "POST",
-        mode: 'no-cors',
-        body: JSON.stringify({text, alphabet, alwaysJ}),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        }
-    }).then(res => {
-        if (res.ok) return res.text();
-        else return 'error';
-    });
+const tarask: Promisify<Tarask> = (
+  text,
+  isHtml,
+  { abc: alphabet, j: alwaysJ },
+) => {
+  return fetch('http://localhost:3000/', {
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify({ text, alphabet, alwaysJ }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((res) => {
+    if (res.ok) return res.text();
+    else return 'error';
+  });
 };
 
 export default tarask;
