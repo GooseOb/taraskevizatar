53f35fa8be74602e4f63c489d07d58619a28e7d1
nopears <gregorzhilich@gmail.com>
2023-04-03 22:34:12 +0300

    Added second e2e test

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -27,3 +27,31 @@ describe('Converter without HTML (e2e)', () => {
       .expect('Чэскі й польскі, Джоньні, асяродзьдзе')
   })
 })
+
+describe('Converter with HTML (e2e)', () => {
+  let app: INestApplication
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+    await app.init()
+  })
+
+  it('/ (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/')
+      .send({
+        text: 'Чэшскі і польскі, Джонні, асяроддзе',
+        alphabet: 0,
+        alwaysJ: 2,
+        isHtml: true,
+      })
+      .expect(201)
+      .expect(
+        'Ч<tarF>эс</tarF>кі <tarF>й</tarF> польскі, Джон<tarF>ь</tarF>ні, асярод<tarF>зь</tarF>дзе',
+      )
+  })
+})
