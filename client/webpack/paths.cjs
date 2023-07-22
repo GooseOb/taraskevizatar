c6d3232a204125a8d7f738c34fbf312d526002ad
GooseOb <gooose.ob@gmail.com>
2023-07-22 15:00:19 +0300

    fix pre-commit hook

--- client/webpack/paths.cjs
+++ client/webpack/paths.cjs
@@ -1,7 +1,7 @@
 const { resolve } = require('path');
 
-const paths = module.exports = {
+const paths = (module.exports = {
 	root: resolve('..'),
-};
+});
 paths.context = resolve(paths.root, 'client');
 paths.output = resolve(paths.context, 'build');
