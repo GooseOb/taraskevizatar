1d9909f1be15c37a8131b36e972f13fa5b0c1551
GooseOb <gooose.ob@gmail.com>
2024-05-28 19:41:35 +0200

    fix selection color in firefox & optimize css and html

--- client/src/main.ts
+++ client/src/main.ts
@@ -172,7 +172,7 @@ Object.assign(
 	localStorage.tarask_text
 		? {
 				value: localStorage.tarask_text,
-			}
+		  }
 		: {
 				value: __DEFAULT_TEXT__,
 				onclick(this: AppInputElement) {
@@ -182,7 +182,7 @@ Object.assign(
 					this.fixHeight();
 					convert('');
 				},
-			}
+		  }
 );
 
 const forceConversion = () => convert(input.value);
@@ -271,7 +271,7 @@ const actions: Record<Action, () => void> = {
 
 const valueProps = ['innerText', 'value'] satisfies [
 	right: keyof HTMLDivElement,
-	left: keyof HTMLTextAreaElement,
+	left: keyof HTMLTextAreaElement
 ];
 for (const btnBar of document.querySelectorAll<HTMLDivElement>('.icon-btns')) {
 	const textfield = $<HTMLTextAreaElement | HTMLDivElement>(
