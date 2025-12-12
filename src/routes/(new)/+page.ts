26ab2bddb6f29586bc515fa7ffa511dd71354174
GooseOb <gooose.ob@gmail.com>
2025-12-12 22:07:43 +0100

    fix redirected check

--- src/routes/(new)/+page.ts
+++ src/routes/(new)/+page.ts
@@ -2,8 +2,10 @@ import { resolve } from '$app/paths';
 import { redirect } from '@sveltejs/kit';
 
 export const load = () => {
-	if (localStorage.getItem('tarask_text') !== null && !localStorage.getItem('tarask_redirected')) {
+	if (!localStorage.getItem('tarask_redirected')) {
 		localStorage.setItem('tarask_redirected', 'true');
-		redirect(307, resolve('/old'));
+		if (localStorage.getItem('tarask_text') !== null) {
+			redirect(307, resolve('/old'));
+		}
 	}
 };
