0288ddaac595540a2c2b1b872662a3d7f9761f59
GooseOb <gooose.ob@gmail.com>
2025-12-12 21:32:55 +0100

    resolve path in redirect

--- src/routes/(new)/+page.ts
+++ src/routes/(new)/+page.ts
@@ -1,11 +1,9 @@
+import { resolve } from '$app/paths';
 import { redirect } from '@sveltejs/kit';
 
 export const load = () => {
-	if (
-		localStorage.getItem('tarask_text') !== null &&
-		!localStorage.getItem('tarask_redirected')
-	) {
+	if (localStorage.getItem('tarask_text') !== null && !localStorage.getItem('tarask_redirected')) {
 		localStorage.setItem('tarask_redirected', 'true');
-		redirect(307, '/old');
+		redirect(307, resolve('/old'));
 	}
 };
