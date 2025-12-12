34cce915bb1ce8ccfbdc52311e7b035fbed79bbf
GooseOb <gooose.ob@gmail.com>
2025-12-12 20:53:53 +0100

    update github pages configuration

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,12 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+		},
+	},
 };
 
 export default config;
